8bfed3305e720c87023726f93a2d5374312eada5
czeslawzalewski75 <75135939+czeslawzalewski75@users.noreply.github.com>
2021-01-09 16:24:25 +0100

    Add files via upload

---
+++ eko_v1.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import pandas as pd
+import random
+import shap
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+import pickle
+
+st.write("""
+# Ekobilet Machine Learning
+beta -v1
+
+Prognoza **ceny biletu** wg: miasta, typu i dnia wydarzania 
+""")
+st.write('---')
+
+
+# Sidebar
+# Header of Specify Input Parameters
+st.sidebar.header('Określ parametry wejściowe')
+
+
+def user_input_features():
+    typ = st.sidebar.selectbox(
+        "Wybierz typ wydarzenia?",
+        ("Koncert", "Kino")
+    )
+    dzien = st.sidebar.selectbox(
+        "Wybierz dzień wydarzenia",
+        ("W tygodniu", "Weekend")
+    )
+    lokalizacja = st.sidebar.selectbox(
+        "Wybierz miasto",
+        ("Kraków", "Stary Sącz")
+    )
+
+    data = {"Typ":typ,
+            "Dzień": dzien,
+            "Lokalizacja": lokalizacja,
+            }
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+
+
+# Main Panel
+
+
+df = pd.read_pickle('df_row.pickle')
+df_row = df.copy()
+
+
+
+
+
+
+
+# prediction data
+df_prediction = user_input_features()
+
+st.header('Wybrane parametry do prognozy')
+st.write(df_prediction)
+st.write('---')
+
+
+df_prediction["Typ"] = df_prediction["Typ"].replace({
+    "Kino":0,
+    "Koncert":1,
+})
+
+df_prediction["Dzień"] = df_prediction["Dzień"].replace({
+    "W tygodniu":0,
+    "Weekend":1,
+})
+
+df_prediction["Lokalizacja"] = df_prediction["Lokalizacja"].replace({
+    "Stary Sącz":0,
+    "Kraków":1,
+})
+#
+# st.header('Prediction of dataset')
+# st.write(df_prediction)
+
+# Apply Model to Make Prediction
+with open('eko_model.pickle','rb') as f:
+  model = pickle.load(f)
+
+prediction = model.predict(df_prediction)
+
+st.header('Prognoza ceny biletu')
+st.write(f"{round(prediction[0],2)} zł")
+st.write('---')
+
+st.header('Dane użyte treningu modelu')
+st.write(df_row)
+st.write('---')
